1c08e50978e7b1189e2aa7f3829d669eb954c0fc
Sandra-beep <sandra.rivas@medieinstitutet.se>
2021-09-21 14:11:36 +0200

    footer styling

--- wp-content/themes/scootertheme/footer.php
+++ wp-content/themes/scootertheme/footer.php
@@ -1,11 +1,12 @@
 <?php wp_footer(); ?>
 
 <footer id="footer">
+	<hr />
 			<div class="container">
 					<div class="about-us">
 						<h4>Kort om oss</h4>
 						<p>
-                        TheScooterTeam.se säljer scootrar som är effektiva, miljövänliga och billiga transportmedel. <br />
+                        Scooter Haven säljer scootrar som är effektiva, miljövänliga och billiga transportmedel. <br />
                         Letandet efter parkeringsplats är ett minne blott. Vägtullar, bilköer, höga bensinkostnader kommer du inte behöva bry dig om. <br />
                         Med en scooter så kommer du alltid ha en låg totalkostnad
                         </p>
@@ -21,7 +22,7 @@
 							Tel: 0123456789<br />
 							E-post: <a href="">info@scooterhaven.com</a>
 						</p>
-
+						<br />
                         <p> Vardagar: 10:00-18:00<br />
                             Lördagar: 10:00-15:00<br />
                             Söndagar: Stängt
@@ -29,7 +30,6 @@
 					</div>
 					<div class="social-media">
 						<h4>Social media</h4>
-						<ul class="social">
 							<li>
 								<i class="fa fa-facebook"></i> <a href="">Facebook</a>
 							</li>
@@ -42,9 +42,10 @@
 							<li>
 								<i class="fa fa-linkedin"></i> <a href="">LinkedIn</a>
 							</li>
-						</ul>
 					</div>
 				</div>
+				<hr />
+
 				<div class="copyright">
 					<div class="">
 						<p>Copyright &copy; Grupp 1, Medieinstitutet, inriktning E-handel, 2021</p>
