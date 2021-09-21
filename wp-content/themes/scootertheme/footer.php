09c7ee47698c1a67edef97e2be394469d9d3156d
Sandra-beep <sandra.rivas@medieinstitutet.se>
2021-09-21 10:06:51 +0200

    first draft of footer

---
+++ wp-content/themes/scootertheme/footer.php
@@ -0,0 +1,62 @@
+<?php wp_footer(); ?>
+
+<footer id="footer">
+			<div class="container">
+				<div class="">
+					<div class="">
+						<h4>Kort om oss</h4>
+						<p>
+                        TheScooterTeam.se säljer scootrar som är effektiva, miljövänliga och billiga transportmedel. <br />
+                        Letandet efter parkeringsplats är ett minne blott. Vägtullar, bilköer, höga bensinkostnader kommer du inte behöva bry dig om. <br />
+                        Med en moped eller scooter så kommer du alltid ha en låg totalkostnad
+                        </p>
+					</div>
+					<div class="">
+						<h4>Kontaktuppgifter</h4>
+						<p>
+							The Scooter Team<br />
+							Gatgatan 1<br />
+							123 45 Stockholm
+						</p>
+						<p>
+							Tel: 0123456789<br />
+							E-post: <a href="">info@grupp1.com</a>
+						</p>
+
+                        <p> Vardagar: 10:00-18:00<br />
+                            Lördagar: 10:00-15:00<br />
+                            Söndagar: Stängt
+                        </p>
+					</div>
+					<div class="">
+						<h4>Social media</h4>
+						<ul class="social">
+							<li>
+								<i class="fa fa-facebook"></i> <a href="">Facebook</a>
+							</li>
+							<li>
+								<i class="fa fa-twitter"></i> <a href="">Twitter</a>
+							</li>
+							<li>
+								<i class="fa fa-instagram"></i> <a href="">Instagram</a>
+							</li>
+							<li>
+								<i class="fa fa-linkedin"></i> <a href="">LinkedIn</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row bottom">
+					<div class="">
+						<p>Copyright &copy; Grupp 1, Medieinstitutet, inriktning E-handel, 2021</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+        </div>
+
+    <script src="js/script.js"></script>
+
+</body>
+</html>
