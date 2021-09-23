a5af671699932e5f020cf2ec1786f06d61e2184b
DavidSaupe <david.saupe@gmail.com>
2021-09-23 15:29:15 +0200

    prepull

--- wp-content/themes/scootertheme/functions.php
+++ wp-content/themes/scootertheme/functions.php
@@ -56,16 +56,16 @@ register_nav_menus(
 // Our custom post type function
 function create_posttype() {
  
-    register_post_type( 'footer',
+    register_post_type( 'store',
     // CPT Options
         array(
             'labels' => array(
-                'name' => __( 'Footers' ),
-                'singular_name' => __( 'Footer' )
+                'name' => __( 'Stores' ),
+                'singular_name' => __( 'Store' )
             ),
             'public' => true,
             'has_archive' => true,
-            'rewrite' => array('slug' => 'Footers'),
+            'rewrite' => array('slug' => 'Stores'),
             'show_in_rest' => true,
  
         )
@@ -83,17 +83,17 @@ function custom_post_type() {
  
     // Set UI labels for Custom Post Type
         $labels = array(
-            'name'                => _x( 'Footers', 'Post Type General Name', 'labb1-david' ),
-            'singular_name'       => _x( 'Footer', 'Post Type Singular Name', 'labb1-david' ),
-            'menu_name'           => __( 'Footers', 'labb1-david' ),
-            'parent_item_colon'   => __( 'Parent Footer', 'labb1-david' ),
-            'all_items'           => __( 'All Footers', 'labb1-david' ),
-            'view_item'           => __( 'View Footer', 'labb1-david' ),
-            'add_new_item'        => __( 'Add New Footer', 'labb1-david' ),
+            'name'                => _x( 'Stores', 'Post Type General Name', 'labb1-david' ),
+            'singular_name'       => _x( 'Store', 'Post Type Singular Name', 'labb1-david' ),
+            'menu_name'           => __( 'Stores', 'labb1-david' ),
+            'parent_item_colon'   => __( 'Parent Store', 'labb1-david' ),
+            'all_items'           => __( 'All Stores', 'labb1-david' ),
+            'view_item'           => __( 'View Store', 'labb1-david' ),
+            'add_new_item'        => __( 'Add New Store', 'labb1-david' ),
             'add_new'             => __( 'Add New', 'labb1-david' ),
-            'edit_item'           => __( 'Edit Footer', 'labb1-david' ),
-            'update_item'         => __( 'Update Footer', 'labb1-david' ),
-            'search_items'        => __( 'Search Footer', 'labb1-david' ),
+            'edit_item'           => __( 'Edit Store', 'labb1-david' ),
+            'update_item'         => __( 'Update Store', 'labb1-david' ),
+            'search_items'        => __( 'Search Store', 'labb1-david' ),
             'not_found'           => __( 'Not Found', 'labb1-david' ),
             'not_found_in_trash'  => __( 'Not found in Trash', 'labb1-david' ),
         );
@@ -101,8 +101,8 @@ function custom_post_type() {
     // Set other options for Custom Post Type
          
         $args = array(
-            'label'               => __( 'Footers', 'labb1-david' ),
-            'description'         => __( 'Footer news and reviews', 'labb1-david' ),
+            'label'               => __( 'Stores', 'labb1-david' ),
+            'description'         => __( 'Store news and reviews', 'labb1-david' ),
             'labels'              => $labels,
             // Features this CPT supports in Post Editor
             'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
@@ -129,7 +129,7 @@ function custom_post_type() {
         );
          
         // Registering your Custom Post Type
-        register_post_type( 'footer', $args );
+        register_post_type( 'store', $args );
      
     }
      
@@ -140,5 +140,6 @@ function custom_post_type() {
      
     add_action( 'init', 'custom_post_type', 0 );
 
+   
 
 ?>
--- wp-content/themes/scootertheme/woocomerce.php
+++ wp-content/themes/scootertheme/woocomerce.php
@@ -7,6 +7,12 @@
         <divclass="row">
             <divclass="col-12">
                 <?php woocommerce_content(); ?>
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+						
+						<h4><?= $stores['title']; ?></h4>
+						
+				</div>
                 </div>
                 </div>
                 </div>
