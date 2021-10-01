5bfcfe5279815c41da443a48f8b4344075ec0093
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2021-10-01 11:49:57 +0200

    sidebar fixed

--- wp-content/themes/scootertheme/front-page.php
+++ wp-content/themes/scootertheme/front-page.php
@@ -19,12 +19,13 @@ echo do_shortcode('[smartslider3 slider="5"]'); //storrea slider
 ?>
 
 
+
+</div>
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
     <div id="secondary" class="widget-area" role="complementary">
     <?php dynamic_sidebar( 'sidebar-1' ); ?>
     </div>
 <?php endif; ?>
-
 </div>
 
 <?php get_footer(); ?>
